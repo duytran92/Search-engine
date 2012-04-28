a11b8c9af2ccb4f70ff09f8ae33f82d45416a4e1
duytran92 <superduy.bkict@gmail.com>
2012-04-29 01:14:12 +0700

    Update indexer.h

--- indexer.h
+++ indexer.h
@@ -1,10 +1,44 @@
 #ifndef INDEXER_H
 #define INDEXER_H
+#include "inode.h"
+#include <set>
+#include <stack>
+#include <vector>
+
+enum Status {ERROR_SYNTAX, WARNING_STOPWORD, SUCCESS};
 
 class Indexer
 {
+protected:
+    INode * indexer_;
+    set<string> stopwords_;
+    stack<string> query_;
+    vector<Document> result_;
+    Status status_;
+
 public:
     Indexer();
+    ~Indexer();
+    void insertKey(const string & keyword);
+    static INode * insertKey(INode * node, const string & keyword);
+    static INode * reBalance(INode * node);
+    INode * at(const string & keyword);
+    void setQuery(const string &query);
+    void execute();
+    vector<Document> result();
+    void addDocument(const string &docname);
+    vector<Document> operator[](const string &keyword);
+    bool isIgnore(const string &keyword);
+    static bool isGarbage(char c);
+    static void filter (string &keyword);
+    void indexStopWords(const string &wordfile);
+    // For debugging
+    static void traverse(INode * node);
+    INode * indexer();
+    
+    Status status();
+    static vector<Document> match(INode * node, string regex);
+    static vector<Document> match(INode * node, vector<string> q_regex);
 };
 
 #endif // INDEXER_H
